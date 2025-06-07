026978cdf06949a11f8d4c4e0913c2d6208ec983
alvinisaac0602 <kiizaisaacalvin256@gmail.com>
2025-06-07 08:00:52 +0300

    pre installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import cors from "cors"; // ✅ Import cors
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Err:" + err));
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -32,6 +35,12 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // ✅ Error handler
 app.use((err, req, res, next) => {

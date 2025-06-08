f7baef2b24b8a24a91ad641126d8995ba4e61ae0
alvinisaac0602 <kiizaisaacalvin256@gmail.com>
2025-06-08 16:15:01 +0300

    final

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use("/api/listing", listingRouter);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
